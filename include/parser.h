a64b8922e7ffda1777b6354423f31e058edbb411
PibeCaverna <francoafalconieri@gmail.com>
2025-07-24 15:52:46 -0300

    Definiciones de estructuras y funciones del parser

---
+++ include/parser.h
@@ -0,0 +1,23 @@
+/* Ver Archivo LICENSE Para detalles sobre la licencia*/
+
+/*Estructura para caracterizar operaciones*/
+typedef struct Operacion{
+  char *cadena;
+  unsigned int orden;
+  Operacion *argumento;
+} operacion;
+
+/*Estructura para caracterizar procedimientos o bloques de código*/
+typedef struct Procedimiento{
+  char *cadena;
+  unsigned int orden;
+  Operacion *listaop;
+  Operacion *argumento;
+  Procedimiento *listaproc;
+} Procedimiento;
+
+/*Función para levantar argumentos y procedimientos*/
+int leer_archivo(char *dir, Procedimiento *procs, Operacion *ops);
+
+/*Función para generar código y excportarlo a un archivo*/
+int generar_archivo(char *dir, Procedimiento *procs, Operacion *ops);
---
+++ src/parser.c
@@ -0,0 +1,14 @@
+/* Ver Archivo LICENSE Para detalles sobre la licencia*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "parser.h"
+
+static char *
+generar_operacion(){
+}
+
+static char *
+generar_procedimiento{
+}
